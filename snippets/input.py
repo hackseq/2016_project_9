e2301b3a70d64be9b3453d628f7a24d657aa64c5
Tommy Carstensen <tc9@sanger.ac.uk>
2016-10-15 15:21:19 -0700

    Update input.py
    Added help on each argument.

--- snippets/input.py
+++ snippets/input.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Provide VCF with optional required SNPs and blacklisted SNPS')
 parser.add_argument('--vcf', required=True)
-parser.add_argument('--reqSNP', required=False)
-parser.add_argument('--blacklist', required=False)
+parser.add_argument('--reqSNP', required=False, help='Text file with pre-selected SNPs with one SNP ID per line')
+parser.add_argument('--blacklist', required=False, help='SNPs not to include in the set of candidate SNPs')
 
 args = parser.parse_args();
