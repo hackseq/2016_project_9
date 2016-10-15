321d39e88b1786ae5aa8ceb6afc995bc15ae4c93
Jocelyn <jocelynjyl@users.noreply.github.com>
2016-10-15 12:10:14 -0700

    added input argparse

---
+++ python/input.py
@@ -0,0 +1,9 @@
+#! python
+import argparse
+
+parser = argparse.ArgumentParser(description='Provide VCF with optional required SNPs and blacklisted SNPS')
+parser.add_argument('--vcf', required=True)
+parser.add_argument('--reqSNP', required=False)
+parser.add_argument('--blacklist', required=False)
+
+args = parser.parse_args();
