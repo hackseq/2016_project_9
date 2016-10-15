97b076f7d2a49da35f6cc1354d94d5e3cb3e7510
Jocelyn <jocelynjyl@users.noreply.github.com>
2016-10-15 14:54:10 -0700

    Reorganized files

--- python/input.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import argparse
-
-parser = argparse.ArgumentParser(description='Provide VCF with optional required SNPs and blacklisted SNPS')
-parser.add_argument('--vcf', required=True)
-parser.add_argument('--reqSNP', required=False)
-parser.add_argument('--blacklist', required=False)
-
-args = parser.parse_args();
