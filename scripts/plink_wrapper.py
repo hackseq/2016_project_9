fe39efd3a00da24283e4353d2d9aff37fa1ebeab
awreynolds <awreynolds@utexas.edu>
2016-10-16 17:54:29 -0500

    working on chr bug

--- scripts/plink_wrapper.py
+++ scripts/plink_wrapper.py
@@ -23,7 +23,7 @@ def main():
         numchr=args.chrnum
     else:
         chr = ""
-        numchr="1"
+        numchr=1
         
     #populate and run plink commands
     for vcf in args.vcf:    
