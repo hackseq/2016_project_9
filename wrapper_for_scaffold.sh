c1238474320978ae2f5cc1a2580910d9480c2f4c
Tommy Carstensen <tommy.carstensen@gmail.com>
2016-10-16 10:03:33 -0700

    Shell wrapper script for scaffold.py.

---
+++ wrapper_for_scaffold.sh
@@ -0,0 +1,9 @@
+python3 \
+ scaffold.py \
+  --in ESN.txt YRI.txt LWK.txt MSL.txt GWD.txt \
+  --out test \
+  --min_LD 0.8 \
+  --min_MAF 0.02 \
+  --max_window 200000 \
+  --max_tagSNP 2000000 \
+
