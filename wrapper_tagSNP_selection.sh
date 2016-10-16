8a1fbcf93a8a14e07c9a3d3d45c1ecb15444fbb4
Tommy Carstensen <tommy.carstensen@gmail.com>
2016-10-16 15:43:36 -0700

    Fixed a tiny file output path name aestethical issue.

--- wrapper_tagSNP_selection.sh
+++ wrapper_tagSNP_selection.sh
@@ -3,7 +3,7 @@
 python3 \
  2016_project_9/scaffold.py \
   --in ESN.txt YRI.txt LWK.txt MSL.txt GWD.txt \
-  --out chrom20.$max_tagSNP \
+  --out chrom20 \
   --min_LD 0.8 \
   --min_MAF 0.02 \
   --max_window 200000 \
