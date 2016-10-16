4707c03143da9504dcd68dda7bfb4e8e88b1ba7c
Ayton Meintjes <aytonm@gmail.com>
2016-10-17 01:20:06 +0200

    run full set

--- imputation/run.sh
+++ imputation/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-INFOLDER=input/
+#INFOLDER=test_input/
+INFOLDER=input
 OUTPUTROOT=output
 
 set -e 
 
-for POPNAME in XXX ESN GWD LWK MSL YRI
+for POPNAME in NONE ESN GWD LWK MSL YRI
+#for POPNAME in NONE ESN
 do
 OUTFOLDER=${OUTPUTROOT}/1000GP_Phase3_minus_${POPNAME}
 mkdir -p ${OUTFOLDER}
-python ./make_impute2_refset.py -t 24 -p ${POPNAME} -i ${INFOLDER} -o ${OUTFOLDER} &
+python ./make_impute2_refset.py -t 24 -p ${POPNAME} -i ${INFOLDER} -o ${OUTFOLDER}
 done
 exit
